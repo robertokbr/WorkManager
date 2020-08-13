51df913d4c732c75ef5e4a193cf654c946704c0e
Roberto Junior <robertojuniordev@gmail.com>
2020-08-13 18:45:11 -0300

    Init Project

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import routes from './routes';
+import './database';
+import AppError from './errors/AppError';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server error',
+  });
+});
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`server started on port 3333 at `);
+});
