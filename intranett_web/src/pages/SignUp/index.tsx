f878b2d93792f18a7f7c179a0a60af01136f7ed8
Roberto Junior <robertojuniordev@gmail.com>
2020-08-21 11:35:28 -0300

    Add SignUp Page

--- intranett_web/src/pages/SignUp/index.tsx
+++ intranett_web/src/pages/SignUp/index.tsx
@@ -1,29 +1,30 @@
 import React, { useCallback, useRef } from 'react';
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FiArrowLeft, FiLock, FiUser } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
 import { Form } from '@unform/web';
-import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 import { Container, Background, Content, AnimationContainer } from './styles';
-import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import api from '../../services/api';
 
 interface SignUpFormData {
   name: string;
-  email: string;
   password: string;
 }
 
 const SignUp: React.FC = () => {
   const history = useHistory();
-
   const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(
     async (data: SignUpFormData) => {
-      await api.post('/users', data);
-      history.push('/');
+      try {
+        await api.post('/user', data);
+        history.push('/');
+      } catch (err) {
+        alert('Dados invalidos');
+      }
     },
 
     [history],
@@ -38,7 +39,6 @@ const SignUp: React.FC = () => {
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu Cadastro</h1>
             <Input icon={FiUser} name="name" placeholder="Nome" />
-            <Input icon={FiMail} name="email" placeholder="E-mail" />
             <Input
               icon={FiLock}
               name="password"
