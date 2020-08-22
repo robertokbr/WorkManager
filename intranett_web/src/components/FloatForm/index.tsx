164768b6f94d8e909235020cb325131812de30b5
Roberto Junior <robertojuniordev@gmail.com>
2020-08-22 00:32:28 -0300

    Add component FloatForm to all task handle

--- intranett_web/src/components/FloatForm/index.tsx
+++ intranett_web/src/components/FloatForm/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { useRef, useCallback } from 'react';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -9,9 +10,10 @@ import { useAuth } from '../../hooks/auth';
 
 interface TaskData {
   name: string;
-  date: Date;
-  time: Date;
+  date?: Date;
+  time?: Date;
 }
+
 interface TaskContent {
   id: string;
   name: string;
@@ -21,12 +23,20 @@ interface TaskContent {
   finished_at: Date;
   cancellationReason: string;
 }
-interface Response {
-  returnTask: (task: TaskContent) => void;
+
+interface TaskOperation {
+  task?: TaskContent;
+  operation: 'addTask' | 'detailTask' | 'cancelTask' | 'finishTask';
 }
 
-const FloatForm: React.FC<Response> = ({ children, returnTask }) => {
+interface Props {
+  returnTask: (task: TaskContent) => void;
+  taskFunction: TaskOperation;
+}
+
+const FloatForm: React.FC<Props> = ({ children, returnTask, taskFunction }) => {
   const { token } = useAuth();
+  const { task } = taskFunction;
   const formRef = useRef<FormHandles>(null);
   const handleSubmit = useCallback(
     async (data: TaskData) => {
@@ -52,18 +62,129 @@ const FloatForm: React.FC<Response> = ({ children, returnTask }) => {
     },
     [token, returnTask],
   );
+  const handleGetFormatedData = useCallback((data: Date) => {
+    if (!data) return;
+    const localDate = new Date(data).toLocaleDateString('pt-br');
+    const localTime = new Date(data).toLocaleTimeString('pt-br');
+    const completDate = `${localDate} as ${localTime}`;
+    return completDate;
+  }, []);
+
+  const handleUpdate = useCallback(
+    async data => {
+      const formatedDate = new Date(`${data.date} ${data.time}`);
+
+      try {
+        await api
+          .put(
+            '/task',
+            {
+              task_id: task?.id,
+              finished_at: formatedDate,
+              cancellationReason: data.cancel,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            },
+          )
+          .then(response => returnTask(response.data));
+      } catch (err) {
+        alert('Dados invalidos');
+      }
+    },
+    [token, returnTask, task],
+  );
+
+  if (task) {
+    return (
+      <Container>
+        {taskFunction.operation === 'cancelTask' && (
+          <FloatFormContainer>
+            <h2>
+              Cancelar Tarefa
+              {taskFunction.task?.name}
+            </h2>
+            <strong>
+              Início: <span>{task.started_at}</span>
+            </strong>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input name="date" type="Date" />
+              <Input name="time" type="Time" />
+              <Input name="reason" placeholder="Motivo" />
+              <Button type="submit">Adicionar</Button>
+            </Form>
+            {children}
+          </FloatFormContainer>
+        )}
+        {taskFunction.operation === 'detailTask' && (
+          <FloatFormContainer>
+            <div>
+              <h2>
+                {task?.name} - {task?.status}
+              </h2>
+              <strong>
+                Início: <span>{handleGetFormatedData(task.started_at)}</span>
+              </strong>
+              <strong>
+                Término:
+                <span>
+                  {task.finished_at
+                    ? handleGetFormatedData(task.finished_at)
+                    : 'Tarefa em andamento'}
+                </span>
+              </strong>
+              {task.cancellationReason && (
+                <strong>
+                  Motivo: <span>{task.cancellationReason}</span>
+                </strong>
+              )}
+            </div>
+            {task.status === 'Andamento' && (
+              <Button type="button" onClick={handleUpdate}>
+                Finalizar
+              </Button>
+            )}
+
+            {children}
+          </FloatFormContainer>
+        )}
+        {taskFunction.operation === 'finishTask' && (
+          <FloatFormContainer>
+            <div>
+              <h2>{task?.name} - Andamento</h2>
+              <strong>
+                Início: <span>{handleGetFormatedData(task.started_at)}</span>
+              </strong>
+            </div>
+            <Input name="date" type="Date" />
+            <Input name="time" type="Time" />
+            <Button type="button" onClick={handleUpdate}>
+              Finalizar
+            </Button>
+
+            {children}
+          </FloatFormContainer>
+        )}
+      </Container>
+    );
+  }
+
   return (
     <Container>
-      <FloatFormContainer>
-        <h1>Nova tarefa</h1>
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <Input name="name" placeholder="Nome da tarefa" />
-          <Input name="date" type="Date" />
-          <Input name="time" type="Time" />
-          <Button type="submit">Adicionar</Button>
-        </Form>
-        {children}
-      </FloatFormContainer>
+      {taskFunction.operation === 'addTask' && (
+        <FloatFormContainer>
+          <h1>Nova tarefa</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <Input name="name" placeholder="Nome da tarefa" />
+            <Input name="date" type="Date" />
+            <Input name="time" type="Time" />
+            <Button type="submit">Adicionar</Button>
+          </Form>
+          {children}
+        </FloatFormContainer>
+      )}
     </Container>
   );
 };
--- intranett_web/src/components/FloatForm/styles.ts
+++ intranett_web/src/components/FloatForm/styles.ts
@@ -3,9 +3,11 @@ import { motion } from 'framer-motion';
 import { shade } from 'polished';
 
 export const Container = styled(motion.div)`
-  position: absolute;
-  height: 100%;
-  width: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -13,16 +15,27 @@ export const Container = styled(motion.div)`
 `;
 
 export const FloatFormContainer = styled.div`
-  height: 35rem;
-  width: 30rem;
+  padding: 2rem 5rem;
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
   flex-direction: column;
   background: #282a36;
   color: #fff;
-  border-radius: 10px;
+  border-radius: 5px;
 
+  h1,
+  h2 {
+    margin-bottom: 3rem;
+  }
+  strong {
+    margin-bottom: 1rem;
+    display: block;
+    span {
+      margin-left: 1rem;
+      font-weight: 400;
+      opacity: 0.8;
+    }
+  }
   button {
     width: 300px;
   }
@@ -34,8 +47,10 @@ export const FloatFormContainer = styled.div`
     align-items: center;
     justify-content: center;
     transition: 0.2s;
+    font-weight: bold;
+    margin-top: 2rem;
     svg {
-      margin-left: 10px;
+      margin-left: 5px;
     }
     &:hover {
       color: ${shade(0.5, '#fff')};
