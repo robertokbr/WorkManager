effbf7fce4dd4f3e9e1f6025af7a5e30bbc1eb8b
robertokbr <robertojuniordev@gmail.com>
2021-02-06 15:25:31 -0300

    Separate backend and frontend by repository

--- Backend/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: '277b2cebde8a62e227ac7b1e5ed2a4e5',
-    expiresIn: '1d',
-  },
-};
--- Frontend/src/components/Loading/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  background: #121212ff;
-  align-items: center;
-  justify-content: center;
-`;
